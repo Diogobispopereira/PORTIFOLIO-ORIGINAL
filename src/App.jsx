f3d14a23116abea80f75a41efd80da770aead47c
diogobispopereira <diogobispopereira>
2025-05-18 16:46:31 -0300

    atualizacao no codigo 12

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Projetos } from './componentes-links/Paginas/Projetos'
 import { Contato } from './componentes-links/Paginas/Contato'
-import { PaginaInicial } from './componentes-links/Paginas/PaginaInicial/paginaInicial'
+
 import { Rodape } from './componentes-links/Rodape/Rodape'
 
 import { Html } from './Componentes/Html'
@@ -12,6 +12,7 @@ import { Css } from './Componentes/Css'
 import { Imersao } from './Componentes/Imersao'
 import { PaginaLink } from './componentes-links/CabeicalhoLinks'
 import { ReactPage } from './Componentes/React'
+import { PaginaInicial } from './componentes-links/Paginas/PaginaInicial'
 
 function App() {
 
@@ -21,13 +22,13 @@ function App() {
         <PaginaLink/>
       <Routes>
         <Route path='/' element={<PaginaInicial/>} />
-        <Route path='/Projetos' element={<Projetos/>} />
-        <Route path='/Contatos' element={<Contato/>} />
-        <Route path='/React' element={<ReactPage/>} />
-        <Route path='/Html' element={<Html/>} />
-        <Route path='/Css' element={<Css/>} />
-        <Route path='/JavaScript' element={<JavaScript/>} />
-        <Route path='/Imersão' element={<Imersao/>} />
+        <Route path='/projetos' element={<Projetos/>} />
+        <Route path='/contatos' element={<Contato/>} />
+        <Route path='/react' element={<ReactPage/>} />
+        <Route path='/html' element={<Html/>} />
+        <Route path='/css' element={<Css/>} />
+        <Route path='/javaScript' element={<JavaScript/>} />
+        <Route path='/imersão' element={<Imersao/>} />
       </Routes>
       <Rodape/>
     </BrowserRouter>
