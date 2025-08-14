0dbff60cd65d7a5a32176d7731acb6423c7eecac
diogobispopereira <diogobispopereira>
2025-08-14 17:44:06 -0300

    adcionando os alt das imagens

--- src/componentes-links/Paginas/Projetos/index.jsx
+++ src/componentes-links/Paginas/Projetos/index.jsx
@@ -6,14 +6,15 @@ export function Projetos() {
     {
       id: 1,
       imagem: "./projetos/AluraFlix.png",
+      alt:"Imagem da página inicial do site, onde mostra os vídeos",
       titulo: "AluraFlix",
       paragrafo: "Este foi desenvolvido em @vite/React. Foi utilizad:Html, Css e JavaScript,neste site você pode adcionar qualquer video, tipo: Música,linguagem de programação etc.",
-      // link: "https://mm-curso.vercel.app/"
       link: "https://alura-flix-seven-beta.vercel.app/"
     },
     {
       id: 2,
       imagem: "./projetos/AluraGeek.png",
+      alt:"Imagem da página inicial do site, onde mostra os cards de carros",
       titulo: "AluraGeek",
       paragrafo: "Este foi desenvolvido 100% em vanila a linguagem principal foi javaScript,neste site de vendas você pode adcionar qualquer coisa desde frutas a carros. Neste modelo foi feito para venda de carros.",
       link: "https://chelleger-alura-geek.vercel.app/"
@@ -22,6 +23,7 @@ export function Projetos() {
     {
       id: 4,
       imagem: "./projetos/decodificador.png",
+      alt:"Imagem da página inicial do site",
       titulo: "Decodificador de texto",
       paragrafo: "Este foi desenvolvido 100% em vanila a linguagem principal foi javaScript,neste site você pode escrever um texto e apertar em decodificar que terá o texto modificado, se reverter o processo ele voltará ao normal .",
       link: "https://codificador-e-decodificado.vercel.app/"
@@ -29,6 +31,7 @@ export function Projetos() {
     {
       id: 5,
       imagem: "./projetos/jogo do numero secreto.png",
+      alt:"Imagem da página inicial do site, onde mostra a caixinha para digitar o número",
       titulo: "Jogo do numero secreto",
       paragrafo: "Este foi desenvolvido 100% em vanila a linguagem principal foi javaScript,neste site você vai se divertir ao tentar acertar o número secreto.",
       link: "https://jogo-do-numero-secreto-hazel-rho.vercel.app/"
@@ -36,6 +39,7 @@ export function Projetos() {
     {
       id: 6,
       imagem: "./projetos/MM curso.jpeg",
+      alt:"Imagem da página inicial do site de inglês e espanhol",
       titulo: "projetos/MM curso",
       paragrafo: "Este foi desenvolvido 100% em Vite/React a linguagem principal foi javaScript,neste site você poderá escolher os cursos de Inglês espanhol e entrar em contato com seus professores.",
       link: "https://jogo-do-numero-secreto-hazel-rho.vercel.app/"
@@ -44,14 +48,11 @@ export function Projetos() {
   return (
     <article className={styles.projetos}>
       <h2>Projetos Selecionado:</h2>
-
-
       <div className={styles.quadroAzul}></div>
       <section className={styles.projeto__container}>
-
         {projeto.map((item) => (
           <a href={item.link} className={styles.projetosImagem} key={item.id}>
-            <img src={item.imagem} />
+            <img src={item.imagem} alt={item.alt} />
             <h3> {item.titulo}</h3>
 
             <p>{item.paragrafo}</p>
